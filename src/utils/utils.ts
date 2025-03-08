ec9eab077d1099581bcdd4a7e0204816153bece1
Elvis Ayiemba <ayiembaelvis@gmail.com>
2025-03-08 22:48:59 +0300

    update utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -94,7 +94,7 @@ export function isValidTopoJson(data: any): boolean {
 
 // Debounce function
 export function debounce(func: Function, delay: number) {
-    let timeoutId: NodeJS.Timeout | undefined;
+    let timeoutId: ReturnType<typeof setTimeout> | undefined;
     return function (this: any, ...args: any[]) {
         clearTimeout(timeoutId);
         timeoutId = setTimeout(() => {
